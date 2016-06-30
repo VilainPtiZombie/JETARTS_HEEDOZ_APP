2dce70750caa82a01ee90ef342632812d799bfbf
VilainPtiZombie <Maxou@MacBook-Air-de-CATHY.local>
2016-06-30 17:30:52 +0200

    le commit sans nom

--- 2016_06_HEEDOZ_APP/swipe/swipe.php
+++ 2016_06_HEEDOZ_APP/swipe/swipe.php
@@ -1,21 +1,3 @@
-<!doctype html>
-<!--[if IE 8]><html class="ie8 lt-ie10"><![endif]-->
-<!--[if IE 9]><html class="ie9 lt-ie10"><![endif]-->
-<!--[if gt IE 9]><!--><html lang="en"><!--<![endif]-->
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="cleartype" content="on">
-    <meta name="MobileOptimized" content="320">
-    <meta name="HandheldFriendly" content="True">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mocha/1.13.0/mocha.min.css">
-    <link rel="stylesheet" href="swipe/test.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/index.css">
-    <title>Slideout tests</title>
-  </head>
   <body>
 
     <nav id="menu" class="menu">
